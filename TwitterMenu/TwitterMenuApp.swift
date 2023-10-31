5c9f29e80761fb14d68626da0f45c7bc87f7eb9f
Dilara Akdeniz <dilarakdeniz15013@gmail.com>
2023-10-31 14:58:17 +0300

    Initial Commit

---
+++ TwitterMenu/TwitterMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterMenuApp.swift
+//  TwitterMenu
+//
+//  Created by Dilara Akdeniz on 31.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct TwitterMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
